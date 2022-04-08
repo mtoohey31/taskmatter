bc1d7c6c81877e7cf5315b347b19f13e913bfeca
Matthew Toohey <contact@mtoohey.com>
2022-04-08 14:11:55 -0400

    feat: clean up flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,24 @@
 {
-  description = "Application packaged using poetry2nix";
+  description = "Taskmatter";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     {
-      # Nixpkgs overlay providing the application
       overlay = nixpkgs.lib.composeManyExtensions [
         poetry2nix.overlay
-        (final: prev: {
-          # The application
-          taskmatter =
-            prev.poetry2nix.mkPoetryApplication { projectDir = ./.; };
+        (self: super: {
+          taskmatter = super.poetry2nix.mkPoetryApplication { projectDir = ./.; };
         })
       ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-      in {
-        apps = { taskmatter = pkgs.taskmatter; };
-
-        defaultApp = pkgs.taskmatter;
-      }));
+    } // flake-utils.lib.eachDefaultSystem (system: {
+      defaultPackage = (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      }).taskmatter;
+    });
 }
