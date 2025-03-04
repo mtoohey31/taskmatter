3dec50bc06d5dab948ad1038cfe13fe73391282b
Matthew Toohey <contact@mtoohey.com>
2025-03-03 22:54:12 -0500

    remove unnecessary direct Yams dependency

--- Package.swift
+++ Package.swift
@@ -10,9 +10,6 @@ let package = Package(
         .package(
             url: "https://github.com/woodymelling/swift-frontmatter-parsing",
             revision: "b8b635afe8e0b94f75fe3083ea0238da418ca8e8"),
-        .package(
-            url: "https://github.com/jpsim/Yams",
-            from: "5.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -20,7 +17,6 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "FrontmatterParsing", package: "swift-frontmatter-parsing"),
-                .product(name: "Yams", package: "yams"),
             ],
             path: "Sources")
     ]
